bdfbef7e4b2b4c61f02b3d4a960338df1977d23e
iamAbasu7 <antar.basu@zeeve.io>
2022-03-08 11:57:20 +0530

    compile using cargo

---
+++ hello-world-cargo/target/debug/deps/hello_world_cargo-6d01877659ec807d.d
@@ -0,0 +1,5 @@
+/home/antar/rust-program/HelloWorldusingCargo/hello-world-cargo/target/debug/deps/hello_world_cargo-6d01877659ec807d: src/main.rs
+
+/home/antar/rust-program/HelloWorldusingCargo/hello-world-cargo/target/debug/deps/hello_world_cargo-6d01877659ec807d.d: src/main.rs
+
+src/main.rs:
---
+++ hello-world-cargo/target/debug/hello-world-cargo.d
@@ -0,0 +1,1 @@
+/home/antar/rust-program/HelloWorldusingCargo/hello-world-cargo/target/debug/hello-world-cargo: /home/antar/rust-program/HelloWorldusingCargo/hello-world-cargo/src/main.rs
